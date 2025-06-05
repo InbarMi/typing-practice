e8bd2ed436839aa331541bcb36b09e7031a6534e
Inbar Milstein <inbarmilstein@u.boisestate.edu>
2025-06-04 22:49:23 -0600

    display cursor

--- typing-app/src/App.jsx
+++ typing-app/src/App.jsx
@@ -4,12 +4,18 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState('Hello World!');
+  const [textIndex, setTextIndex] = useState(0);
 
   return (
     <>
       <h1>Type What You See :)</h1>
       <div className="text">
-          {text}
+          {text.split('').map((char, index) => (
+              <>
+                  {index === textIndex && <span className="cursor" />}
+                  <span key={index} className={index < textIndex ? 'typed' : ''}>{char}</span>
+              </>
+          ))}
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
