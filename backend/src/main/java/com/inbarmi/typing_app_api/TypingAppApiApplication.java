df163b101117b3f5b1fbee5e92bdf949ad409c91
Inbar Milstein <inbarmilstein@u.boisestate.edu>
2026-02-11 23:30:19 -0700

    move react frontend into /frontend and add spring boot backend skeleton. Configured backend to connect to local PostgreSQL.

---
+++ backend/src/main/java/com/inbarmi/typing_app_api/TypingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.inbarmi.typing_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingAppApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/inbarmi/typing_app_api/TypingAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inbarmi.typing_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
