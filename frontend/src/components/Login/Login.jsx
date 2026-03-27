92f7aa73bdb66e158e09b549612326117eff2a40
Inbar Milstein <inbarmilstein@u.boisestate.edu>
2026-03-26 20:17:58 -0600

    create login component

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,13 +1,26 @@
-import './Login.css'
+import './Login.css';
+import { auth } from '../../firebase';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 /**
- * 
  * Login component to either continue as a guest or login to track stats across sessions
  */
-function Login({ onLogin, onGuest }) {
+function Login({ onGuest, setUserId }) {
+    const handleGoogleLogin = async () => {
+        const provider = new GoogleAuthProvider();
+
+        try {
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+            setUserId(user.uid);
+        } catch (error) {
+            console.error("Login failed: ", error);
+        }
+    }
+
     return (
         <div className="loginContainer">
-            <button onClick={onLogin}>Login Here</button>
+            <button onClick={handleGoogleLogin}>Login with Google Here</button>
             <button onClick={onGuest}>Continue as Guest</button>
         </div>
     )
